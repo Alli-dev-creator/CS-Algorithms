939edc249564ae484c9444fd55330540e9fa91b4
Alli Abdul Quayum <abdulquayumalli@student.oauife.edu.ng>
2025-04-19 19:51:34 +0100

    Add initial implementation of tuple, list, and range examples

---
+++ data_type.py
@@ -0,0 +1,14 @@
+# Tuple: Immutable collection
+my_tuple = (1, 2, 3)
+print("Tuple:", my_tuple)
+print("Accessing tuple element:", my_tuple[1])  # Accessing elements
+
+# List: Mutable collection
+my_list = [4, 5, 6]
+print("List before modification:", my_list)
+my_list.append(7)  # Adding an element
+print("List after modification:", my_list)
+
+# Range: Sequence of numbers
+my_range = range(1, 5)  # Generates numbers from 1 to 4
+print("Range as a list:", list(my_range))  # Converting range to list
