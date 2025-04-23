a199b2f1c3a9598a1dc30de8e44c7bde4613e8a6
Alli Abdul Quayum <abdulquayumalli@student.oauife.edu.ng>
2025-04-23 21:18:00 +0100

    Implement bisection method and comparison with Newton-Raphson method

--- nrasqrt.py
+++ nrasqrt.py
@@ -1,10 +1,35 @@
 def nwt_raph(y):
     epsilon = 0.000001
     x = y/3
+    global steps
+    steps = 1
     if y< 0:
         print("Root does not exist")
     else:
         while x**2 - y >= epsilon:
             x = x - (x**2 - y)/(2*x)
+            steps += 1
     print(x)
-nwt_raph(1546)
+def bisect(value):
+    low = 0
+    high = value
+    global times
+    times = 1 
+    ans = (high + low)/2
+    limiter = 0.000001
+    while abs(ans**2 - value) >= limiter:
+        if ans**2 < value:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low)/2
+        times += 1
+    print(ans)
+def compare(number):
+    print("Newton-Raphson method:")
+    nwt_raph(number)
+    print("Bisection method:")
+    bisect(number)
+    print("Newton-Raphson steps:", steps)
+    print("Bisection steps:", times) 
+compare(56789)       
